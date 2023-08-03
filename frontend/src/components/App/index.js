cee57ab8f38aec8aa0db9177f0d114bdfd1fa75e
sabirmostofa <sabirmostofa@gmail.com>
2023-08-03 18:14:31 +0200

    connecting overlay added until connection is established

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,9 +1,12 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 
 import "./App.css";
 import Register from "../Register";
 import Chatbox from "../Chatbox";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
 import { v4 as uuidv4 } from "uuid";
+import { useRef } from "react";
 
 function App() {
   const [userData, setUserData] = useState({
@@ -11,6 +14,39 @@ function App() {
     name: "",
     connected: false,
   });
+
+  const [connected, setConnected] = useState(false);
+  const [stompClient, setStompClient] = useState(null);
+
+  const clientInit = useRef(false);
+
+  useEffect(() => {
+    console.count("connecting to the server!");
+    connect();
+
+    return () => {
+      console.log("cleaning up");
+      if (stompClient) {
+        stompClient.disconnect();
+      }
+    };
+  }, []);
+
+  const connect = () => {
+    clientInit.current = true;
+    const socket = new SockJS("/chat");
+    const tempstompClient = Stomp.over(socket);
+    tempstompClient.connect(
+      { "client-id": userData.userId },
+      () => {
+        setConnected(true);
+        setStompClient(tempstompClient);
+      },
+      (er) => {
+        console.error(er);
+      }
+    );
+  };
 
   const registerUser = (e) => {
     e.preventDefault();
@@ -24,7 +60,11 @@ function App() {
   };
 
   return userData.connected ? (
-    <Chatbox userData={userData} />
+    <Chatbox
+      userData={userData}
+      stompClient={stompClient}
+      connected={connected}
+    />
   ) : (
     <Register handleUser={handleUser} handleSubmit={registerUser} />
   );
--- frontend/src/components/Chatbox/index.js
+++ frontend/src/components/Chatbox/index.js
@@ -1,15 +1,13 @@
 import { useEffect, useRef, useState } from "react";
-import SockJS from "sockjs-client";
-import Stomp from "stompjs";
+import Overlay from "../Overlay";
 
-var stompClient = null;
-export default function Chatbox({ userData }) {
+export default function Chatbox({ userData, stompClient, connected }) {
   const [msg, setMsg] = useState("");
   const [messages, setMessages] = useState([]);
   const [realTimeMessages, setRealTimeMessages] = useState([]);
   const [errorText, setErrorText] = useState("");
-  const [connected, setConnected] = useState(false);
   const endRef = useRef();
+  const userSubsribed = useRef(false);
 
   const onMessageReceived = (payload) => {
     console.log("userID ", userData.userId);
@@ -18,23 +16,11 @@ export default function Chatbox({ userData }) {
     setRealTimeMessages((messages) => [...messages, message]);
   };
 
-  const onConnected = (connetion) => {
-    console.log("Connected!");
-    console.log(connetion);
-    setConnected(true);
+  const subscribeonConnected = () => {
+    console.log("Connected! from the chatbox");
     stompClient.subscribe("/topic/messages", onMessageReceived, {
       id: userData.userId,
     });
-  };
-
-  const onError = (err) => {
-    console.log("Could not connect:", err);
-    setErrorText("couldn't connect to the chat websocket, check console!");
-  };
-  const connect = () => {
-    let socket = new SockJS("/chat");
-    stompClient = Stomp.over(socket);
-    stompClient.connect({ "client-id": userData.userId }, onConnected, onError);
   };
 
   const sendMessage = (event) => {
@@ -75,70 +61,79 @@ export default function Chatbox({ userData }) {
     endRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
+  useEffect(() => {
+    if (stompClient !== null && connected && !userSubsribed.current) {
+      subscribeonConnected();
+      userSubsribed.current = true;
+    }
+  });
+
   useEffect(() => scrollToBottm, [realTimeMessages, messages]);
   useEffect(() => {
     fetchMessages();
-    connect();
   }, []);
 
   return (
-    <div className="mx-auto  border rounded-md shadow-sm max-w-md mt5vh screen75">
-      {errorText.length > 0 && (
-        <div className="bg-error display">{errorText}</div>
-      )}
-      <div className="h-full overflow-y-auto mst">
-        {messages.map((msg) => (
-          <div className="chat chat-start" key={msg.id}>
-            <div className="chat-header">{msg.sender}</div>
-            <div className="chat-bubble">{msg.message}</div>
+    <div>
+      <Overlay connected={connected} />
+      <div className="mx-auto  border rounded-md shadow-sm max-w-md mt5vh screen75">
+        {errorText.length > 0 && (
+          <div className="bg-error display">{errorText}</div>
+        )}
+        <div className="h-full overflow-y-auto mst">
+          {messages.map((msg) => (
+            <div className="chat chat-start" key={msg.id}>
+              <div className="chat-header">{msg.sender}</div>
+              <div className="chat-bubble">{msg.message}</div>
 
-            <div className="chat-footer text-xs">{msg.timestamp}</div>
-          </div>
-        ))}
-        {realTimeMessages.map((msg) => (
-          <div className="chat chat-start" key={msg.id}>
-            <div className="chat-header">
-              {msg.senderId === userData.userId ? "You" : msg.sender}
+              <div className="chat-footer text-xs">{msg.timestamp}</div>
             </div>
-            <div
-              className={
-                msg.senderId === userData.userId
-                  ? "chat-bubble chat-bubble-primary"
-                  : "chat-bubble"
-              }
-            >
-              {msg.message}
+          ))}
+          {realTimeMessages.map((msg) => (
+            <div className="chat chat-start" key={msg.id}>
+              <div className="chat-header">
+                {msg.senderId === userData.userId ? "You" : msg.sender}
+              </div>
+              <div
+                className={
+                  msg.senderId === userData.userId
+                    ? "chat-bubble chat-bubble-primary"
+                    : "chat-bubble"
+                }
+              >
+                {msg.message}
+              </div>
+
+              <div className="chat-footer text-xs">{msg.timestamp}</div>
             </div>
+          ))}
 
-            <div className="chat-footer text-xs">{msg.timestamp}</div>
-          </div>
-        ))}
+          <div ref={endRef}></div>
+        </div>
 
-        <div ref={endRef}></div>
-      </div>
-
-      {/*  end of all chats  start of send*/}
-      <div className="  bottom-0 shadow-lg py-5">
-        <form
-          className="flex mt-0 p-0 align-middle"
-          onSubmit={sendMessage}
-          method="POST"
-          action="#"
-        >
-          <input
-            type="text"
-            onChange={(event) => setMsg(event.target.value)}
-            value={msg}
-            className="input input-md  w-full focus:outline-none bg-gray-100"
-          />
-          <button
-            type="submit"
-            disabled={!connected}
-            className="text-white bg-gray-600 rounded-r-lg px-6 text-sm"
+        {/*  end of all chats  start of send*/}
+        <div className="  bottom-0 shadow-lg py-5">
+          <form
+            className="flex mt-0 p-0 align-middle"
+            onSubmit={sendMessage}
+            method="POST"
+            action="#"
           >
-            Send
-          </button>
-        </form>
+            <input
+              type="text"
+              onChange={(event) => setMsg(event.target.value)}
+              value={msg}
+              className="input input-md  w-full focus:outline-none bg-gray-100"
+            />
+            <button
+              type="submit"
+              disabled={!connected}
+              className="text-white bg-gray-600 rounded-r-lg px-6 text-sm"
+            >
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
